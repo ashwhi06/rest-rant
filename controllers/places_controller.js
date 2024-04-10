647ffac311dee9038c639623817e748e6c0d5855
ashwhi06 <ashe.white06@gmail.com>
2024-04-10 18:13:06 -0400

    Form data working via post

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
       state: "FL",
       cuisines: "Mexican, American",
       pic: "/images/tapas.jpg",
-      photoCredit: "Alex Haney on Unsplash"
+      photoCredit: "Alex Haney on Unsplash",
     },
     {
       name: "Coding Brunch Cafe",
@@ -17,7 +17,7 @@ router.get("/", (req, res) => {
       state: "GA",
       cuisines: "Coffee, Bakery",
       pic: "/images/brunch.jpg",
-      photoCredit: "Alessandro Alimonti on Unsplash"
+      photoCredit: "Alessandro Alimonti on Unsplash",
     },
     {
       name: "Charleston Bistro",
@@ -25,7 +25,7 @@ router.get("/", (req, res) => {
       state: "SC",
       cuisines: "Beef, Chicken",
       pic: "/images/bistro.jpg",
-      photoCredit: "Mgg Vitchakorn on Unsplash"
+      photoCredit: "Mgg Vitchakorn on Unsplash",
     },
     {
       name: "Sushi By the Sea",
@@ -33,18 +33,22 @@ router.get("/", (req, res) => {
       state: "CA",
       cuisines: "Sushi",
       pic: "/images/sushi.jpg",
-      photoCredit: "Vinicius Benedit on Unsplash"
+      photoCredit: "Vinicius Benedit on Unsplash",
     },
   ];
 
   res.render("places/index", { places }); // This is the response for GET /places (ALL places)
 });
 
-
 //New route for GET /places/new
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
+//Post route for POST /places
+router.post("/", (req, res) => {
+ // console.log(req.body);
+  res.send(" POST /places");
+});
 
 module.exports = router;
